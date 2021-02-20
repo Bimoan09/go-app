0d8910bbee93f7e7fcaf635acade12334d59b145
Bimo Anugrah P <bimoanugrah383@gmail.com>
2021-02-20 19:12:51 +0700

    update

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Bimoan09/goapp
+
+go 1.15
+
+require github.com/Bimoan09/gomodule v0.0.0-20210220120117-35da4887579b // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	gomodule "github.com/Bimoan09/gomodule"
+)
+
+func main() {
+	fmt.Println(gomodule.SayHello())
+}
